d08cc9986062c15825d6daf75dcbb85ecf017123
alif <alifrazanit@gmail.com>
2021-08-21 15:18:37 +0700

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,34 @@
-import { Component, ViewChild, AfterViewInit, Renderer2, ElementRef} from '@angular/core';
+import { Component, ViewChild, AfterViewInit, Renderer2, ElementRef, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
   @ViewChild('divcontainer') divcontainer: any;
 
-  constructor(
-    private render: Renderer2
-  ){
-  }
-  ngAfterViewInit(){
-    //
-    console.log(this.divcontainer)
-  }
-  remove(){
+  public innerWidth: any;
+  ngOnInit() {
 
   }
+  constructor(
+    private render: Renderer2
+  ){ }
+  ngAfterViewInit(){
+    this.innerWidth = window.innerWidth;
+      if(this.innerWidth < 992){
+        this.render.removeClass(this.divcontainer.nativeElement, 'container');
+      }  if(this.innerWidth > 992){
+        this.render.addClass(this.divcontainer.nativeElement, 'container');
+      }
+  }
+
   onResize(e:any){
+   this.resize(e);
+  }
+  resize(e:any){
     if(e.target.innerWidth < 992){
       this.render.removeClass(this.divcontainer.nativeElement, 'container');
     }  if(e.target.innerWidth > 992){

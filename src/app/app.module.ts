3c6248417d4eb3765eeceae112899e8a26102cfe
alif <alifrazanit@gmail.com>
2021-08-21 07:52:02 +0700

    bootsrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ActivityComponent } from './components/side-menu/activity/activity.component';
+import { ChannelsComponent } from './components/side-menu/channels/channels.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SideMenuParentComponent } from './components/side-menu/side-menu-parent/side-menu-parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ActivityComponent,
+    ChannelsComponent,
+    FooterComponent,
+    HomeComponent,
+    SideMenuParentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/side-menu/side-menu-parent/side-menu-parent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu-parent',
+  templateUrl: './side-menu-parent.component.html',
+  styleUrls: ['./side-menu-parent.component.css']
+})
+export class SideMenuParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
